17eaa235e05dfb79432c9594dcafe20bb05690a7
Glenn Lewis <gmlewis@google.com>
2014-09-14 23:33:59 -0700

    Allow package to work with App Engine 'goapp' tool.

--- sha256block_amd64.s
+++ sha256block_amd64.s
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// +build !appengine
+
 //#include "../../../cmd/ld/textflag.h"
 // just use the #define for now since this isn't in the main repo yet.
 #define NOSPLIT 4
